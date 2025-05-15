e03d802c577c38308b9069e01901ca5a49095c49
groundpaku <oo_paku@yahoo.co.jp>
2025-05-15 16:38:06 +0900

    docs: Flet + API連携

--- app.py
+++ app.py
@@ -1,12 +1,24 @@
-from flask import Flask
+import os
+import logging.handlers
+from flask import Flask, request, jsonify
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model import M010User
+import logging
+from config import Config
 
 app = Flask(__name__)
 
+app.logger.setLevel(Config.LOGLEVEL)
+handler = logging.handlers.RotatingFileHandler(filename=os.path.abspath(
+Config.LOGFILE), maxBytes=Config.LOGMAXSIZE, backupCount=Config.LOGBACKUPCOUNT, encoding="utf-8")
+handler.setLevel(Config.LOGLEVEL)
+handler.setFormatter(Config.LOGFORMATTER)
+app.logger.addHandler(handler)
+
 @app.route("/")
 def hello():
+    app.logger.info("hello")
     return "Hello World!"
 
 @app.route("/db")
@@ -33,8 +45,35 @@ def db_access():
         return data_list
     except Exception as e:
         return str(e)
+
+@app.route("/login", methods=['POST'])
+def login():
+    dict_login_user = request.get_json()
     
+    try:
+        # DB接続
+        engine = create_engine(
+               f"postgresql://chat_user01:Chat@172.20.63.23/chat_tto"
+            )
+        # セッション作成
+        factory = sessionmaker(bind=engine)
+        session = factory()
 
-
+        # データ検索
+        instance = session.query(M010User).filter(M010User.name == dict_login_user["name"]).first()
+        if instance is not None:
+            dict_user = {"id": instance.id,
+                         "name": instance.name,
+                         "birthday": instance.birthday,
+                         "address": instance.address,
+                         "deptcode": instance.deptcode}
+        
+            return jsonify(dict_user), 201
+        else:
+            return "User does not exist.", 200
+    except Exception as e:
+        app.logger.debug(e)
+        return str(e)
+    
 if __name__ == "__main__":
     app.run()
--- config.py
+++ config.py
@@ -1,1 +1,23 @@
-DEBUG = True
+import logging
+
+class Config:
+    # デバッグフラグ
+    DEBUG = True
+    # ログレベル
+    LOGLEVEL = logging.DEBUG
+    # API待受ポート
+    SERVER_PORT = 8080
+    # ログファイル名
+    LOGFILE = "/var/log/api/api.log"
+    # ログ書式
+    LOGFORMATTER = logging.Formatter("%(asctime)s: %(threadName)s[%(levelname)-7s] %(message)s")
+    # ログ最大サイズ
+    LOGMAXSIZE = 104857600
+    # ログバックアップ保持数
+    LOGBACKUPCOUNT = 10
+    # DB(MySQL) URL
+    DBURL = "postgresql://10.255.254.23:5432/suma2db_d_shinsei?user=suma2_user01&password=PCN6DhiY" 
+    SQLALCHEMY_DATABASE_URI = DBURL
+    # API待受アドレス
+    SERVER_ADDR = "0.0.0.0"
+    
