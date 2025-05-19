c9b0546b9b047651fb6aca0ccc92c8a49a7f4873
groundpaku <oo_paku@yahoo.co.jp>
2025-05-19 17:00:02 +0900

    docs: update file

--- models/model.py
+++ models/model.py
@@ -9,3 +9,12 @@ class M010User(db.Model):
     birthday = db.Column('birthday', db.TEXT)
     address = db.Column('address', db.TEXT)
     deptcode = db.Column('deptcode', db.TEXT)
+
+class T010LoginBonus(db.Model):
+    __tablename__ = 't010_login_bonus'
+
+    id = db.Column('id', db.VARCHAR(10), primary_key=True)
+    login_count = db.Column('login_count', db.INTEGER)
+    last_login_date = db.Column('last_login_date', db.DATETIME)
+    login_point = db.Column('login_point', db.INTEGER)
+    point_expiry_date = db.Column('point_expiry_date', db.DATETIME)
